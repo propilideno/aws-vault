f12dcc4d9a0f025e847838aa4a092d692531799b
Lucas Almeida <lucas@propi.dev>
2026-03-21 18:01:20 -0300

    fix(cli): honor -d for chained GetSessionToken sessions

--- cli/login.go
+++ cli/login.go
@@ -70,6 +70,7 @@ func ConfigureLoginCommand(app *kingpin.Application, a *AwsVault) {
 	cmd.Action(func(c *kingpin.ParseContext) (err error) {
 		input.Config.MfaPromptMethod = a.PromptDriver(false)
 		input.Config.NonChainedGetSessionTokenDuration = input.SessionDuration
+		input.Config.ChainedGetSessionTokenDuration = input.SessionDuration
 		input.Config.AssumeRoleDuration = input.SessionDuration
 		input.Config.GetFederationTokenDuration = input.SessionDuration
 		keyring, err := a.Keyring()
